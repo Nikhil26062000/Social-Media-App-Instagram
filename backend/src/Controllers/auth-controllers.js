fb039cfe22d8ddee7bab300058745db7e00f2d6b
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-01-27 04:08:15 +0530

    Backend :
    - Password hashing for security
    - Generating token for individual users

--- backend/src/Controllers/auth-controllers.js
+++ backend/src/Controllers/auth-controllers.js
@@ -12,7 +12,11 @@ const Home = async(req, res) => {
         return res.status(409).json({message:"Username already exists!"})
     }
     const userCreated = await User.create(req.body);
-    res.status(201).send(userCreated);
+
+    //* Generating token at time of Signup
+    const token = await userCreated.generateToken();
+    //* Sending response with status and Token
+    res.status(201).send({userCreated,token:token});
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
